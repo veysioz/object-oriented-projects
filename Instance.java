cc7cfd22a4398a9bca8e22ddf6ee408b61678515
veysioz <74527563+veysioz@users.noreply.github.com>
2020-11-23 20:10:25 +0300

    Add files via upload

--- Instance.java
+++ Instance.java
@@ -1,76 +1,26 @@
 
 public class Instance {
-
-	public Instance(Object[] instanceID,Object[] instanceTEXT,Object dataSetID,Object dataSetName,Object instanceType,Object maxNumberOfLabelsPerInstance){
-		this.dataSetID   =dataSetID;
-		this.dataSetName =dataSetName;
-		this.instanceID  =instanceID.clone();//copyied object array
-		this.instanceTEXT=instanceTEXT.clone();//copyied object array
-		this.instanceType=instanceType;
-		this.maxNumberOfLabelsPerInstance=maxNumberOfLabelsPerInstance;
+	private int instanceID;
+	private String instanceText;
+	
+	public Instance(int instanceID, String instanceText){
+		this.instanceID = instanceID;
+		this.instanceText = instanceText;
 	}
 
-	private Object[] instanceID;
-	private Object[] instanceTEXT;
-	private Object dataSetID;
-	private Object dataSetName;
-	private Object instanceType;
-	private Object maxNumberOfLabelsPerInstance;
-
-	public Object[] getInstanceID() {
+	public int getInstanceID() {
 		return instanceID;
 	}
 
-	public void setInstanceID(Object[] instanceID) {
+	public void setInstanceID(int instanceID) {
 		this.instanceID = instanceID;
 	}
 
-
-	public Object[] getInstanceTEXT() {
-		return instanceTEXT;
+	public String getInstanceText() {
+		return instanceText;
 	}
 
-
-	public void setInstanceTEXT(Object[] instanceTEXT) {
-		this.instanceTEXT = instanceTEXT;
+	public void setInstanceText(String instanceText) {
+		this.instanceText = instanceText;
 	}
-
-	public Object getDataSetID() {
-		return dataSetID;
-	}
-
-	public void setDataSetID(Object dataSetID) {
-		this.dataSetID = dataSetID;
-	}
-
-	public Object getDataSetName() {
-		return dataSetName;
-	}
-
-	public void setDataSetName(String dataSetName) {
-		this.dataSetName = dataSetName;
-	}
-
-	public Object getInstanceType() {
-		return instanceType;
-	}
-
-	public void setInstanceType(String instanceType) {
-		this.instanceType = instanceType;
-	}
-
-	public Object getMaxNumberOfLabelsPerInstance() {
-		return maxNumberOfLabelsPerInstance;
-	}
-
-	public void setMaxNumberOfLabelsPerInstance(int maxNumberOfLabelsPerInstance) {
-		this.maxNumberOfLabelsPerInstance = maxNumberOfLabelsPerInstance;
-	}
-
-	public static void main(String[] args) {
-
-
-
-	}
-
 }
