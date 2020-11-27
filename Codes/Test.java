306a384fc73a7618078944d01870d9f853080b29
mikailTorun <33750318+mikailTorun@users.noreply.github.com>
2020-11-27 20:55:03 +0300

    Add files via upload
    Creatig and writing to JSON file finished , function name is :writeJsonToFile()

--- Codes/Test.java
+++ Codes/Test.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -5,18 +8,19 @@ import java.util.Date;
 
 public class Test{
 	static ArrayList<User> users = new ArrayList<User>();
-	
+	static FileWriter fileWrite;
+
 	@SuppressWarnings({ "unused", "resource" })
 	public static void main(String[] args) {
 		Dataset dataset = new Dataset();
 		Scanner scanner = new Scanner(System.in);
 		int input = 0, exitNum, userID;
-		
+
 		System.out.println("Welcome to Data Labelling System!");
 		while(true){
 			if(Labelling.insNum == Dataset.arListIns.size() - 1)
 				break;
-			
+
 			if(input == 0) {
 				System.out.println("\n" + Dataset.arListIns.get(Labelling.insNum + 1).getInstanceText());
 				System.out.print("   1 => Random Type\n   2 => Exit\n Your Input: ");
@@ -26,9 +30,9 @@ public class Test{
 				System.out.print("   1 => Random Type(New User)\n   2 => Random Type(Same User)\n   3 => Ramdom Type(Registered User)\n   4 => Exit\n Your Input: ");
 				exitNum = 4;
 			}
-			
+
 			input = scanner.nextInt();
-			
+
 			if(input == exitNum) {
 				break;
 			} else if(input == 1) {
@@ -43,13 +47,13 @@ public class Test{
 				new RandomLabelling(users.get(userID - 1), new Date());
 			}
 		}
-		
+
 		printOutput();
 	}
-	
+	static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy, HH:mm:ss");
 	public static void printOutput() {
-		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy, HH:mm:ss");
-		
+
+
 		System.out.println("\nDataset ID: " + Dataset.datasetID);
 		System.out.println("Dataset Name: " + Dataset.datasetName);
 		System.out.println("Maximum Number of Labels per Instance: " + Dataset.maxNumLabsPerIns);
@@ -69,6 +73,103 @@ public class Test{
 		System.out.print("Users:\n");
 		for(int i = 0; i < users.size(); i++) {
 			System.out.println("\tUser ID: " + users.get(i).getUserID() + " | Username: " + users.get(i).getUserName() + " | User Type: " + users.get(i).getUserType());
+		}
+		try {
+			writeJsonToFile();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+	static int counter ;
+	static int k;
+	private static void writeJsonToFile() throws IOException{
+
+		File file = new File("OutputFile.json");
+		file.createNewFile();
+
+		fileWrite = new FileWriter(file);
+
+		fileWrite.write("{\n\"Dataset ID\": " +Dataset.datasetID+",\n");
+
+		fileWrite.write("\"Dataset Name\": " + "\""+Dataset.datasetName+"\""+",");fileWrite.flush();
+		fileWrite.write("\"Maximum Number of Labels per Instance\": " + Dataset.maxNumLabsPerIns+",");fileWrite.flush();
+
+		fileWrite.write("\n\"Class Labels\":[\n");fileWrite.flush();
+		counter=1;
+		Dataset.arListLab.forEach((n) -> {
+			try {
+				if(Dataset.arListLab.size() != counter){
+					fileWrite.write("{\"Label ID\": " + n.getLabelID() + ", \"Label Text\": " +"\""+ n.getLabelText()+"\"},\n");fileWrite.flush();
+				}else{
+					fileWrite.write("{\"Label ID\": " + n.getLabelID() + ", \"Label Text\": " +"\""+ n.getLabelText()+"\"}");fileWrite.flush();
+
+				}
+				counter++;
+
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		});
+		fileWrite.write("\n],");fileWrite.flush();
+
+		fileWrite.write("\n\"Instances\":[\n");fileWrite.flush();
+		counter =1;
+		Dataset.arListIns.forEach((n) -> {
+			try {
+				if(Dataset.arListIns.size() != counter){
+					fileWrite.write("{\"Instance ID\":" + n.getInstanceID() + ", \"Instance Text\": \"" + n.getInstanceText()+"\"},\n");fileWrite.flush();
+				}else{
+					fileWrite.write("{\"Instance ID\":" + n.getInstanceID() + ", \"Instance Text\": \"" + n.getInstanceText()+"\"}\n],");fileWrite.flush();
+
+				}
+				counter++;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		fileWrite.write("\n\"Class Label Assignments\":[\n");fileWrite.flush();
+
+		for(int i = 0; i < Dataset.arListIns.size(); i++) {
+			fileWrite.write("{\"Instance ID\": " + Dataset.arListIns.get(i).getInstanceID() + ", \"Class Label IDs\": [");fileWrite.flush();
+			counter=1;
+			k=i;
+			Dataset.arListIns.get(i).getAssignedLabels().forEach((n) -> {
+
+				try {
+
+					if(Dataset.arListIns.get(k).getAssignedLabels().size() !=counter){
+						fileWrite.write(n.getLabelID() + " ,");fileWrite.flush();
+
+					}else{
+						fileWrite.write(n.getLabelID() + "],");fileWrite.flush();
+
+					}
+					counter++;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			fileWrite.write("\"User ID\": " + Dataset.arListIns.get(i).getUser().getUserID()+", ");fileWrite.flush();
+			if(i != Dataset.arListIns.size() - 1){
+				fileWrite.write("\"Date & Time\": \"" +  formatter.format(Dataset.arListIns.get(i).getDate())+"\"},\n");fileWrite.flush();
+			}else{
+				fileWrite.write("\"Date & Time\": \"" +  formatter.format(Dataset.arListIns.get(i).getDate())+"\"}\n],");fileWrite.flush();
+			}
+		}
+
+		fileWrite.write("\n\"Users\":[\n");fileWrite.flush();
+		for( int i = 0; i < users.size(); i++) {
+			if(i == users.size()-1){
+				fileWrite.write("{\"User ID\": " + users.get(i).getUserID() + ", \"Username\": \"" + users.get(i).getUserName() + "\", \"User Type\": \"" + users.get(i).getUserType()+"\"}]\n}");
+				fileWrite.flush();
+			}else{
+				fileWrite.write("{\"User ID\": " + users.get(i).getUserID() + ", \"Username\": \"" + users.get(i).getUserName() + "\", \"User Type\": \"" + users.get(i).getUserType()+"\"},\n");
+				fileWrite.flush();
+			}
+
+
 		}
 	}
 }
