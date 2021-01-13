aea2e0423557b317852424c53a165fa54e59ea43
veysioz <74527563+veysioz@users.noreply.github.com>
2021-01-13 14:06:37 +0300

    Add files via upload

--- Python Project/Codes/main.py
+++ Python Project/Codes/main.py
@@ -2,12 +2,17 @@ from classes import *
 
 student_list = StudentList('CES3063_Fall2020_rptSinifListesi.XLS')
 
-reports = Reports('zoom_poll_reports', student_list)
-reports.read_reports()
+Reports('zoom_poll_reports', student_list).read_reports()
 
 results = Results(student_list.get_students())
 
-attendance = Attendance(student_list.get_students(), results)
-attendance.add_attendance()
+Attendance(student_list.get_students(), results).add_attendance()
+
+answer_keys = []
+#answer_keys = AnswerKeys('answer_keys').get_answer_keys()
+for answer_key in answer_keys:
+    print(answer_key.get_poll_name(), ":")
+    for question in answer_key:
+        print(question.get_question(), question.get_answer())
 
 results.save_book()
