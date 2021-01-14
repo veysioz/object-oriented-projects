450e0490856fa621db857337f9f05beb31504453
mikailTorun <33750318+mikailTorun@users.noreply.github.com>
2021-01-15 00:40:02 +0300

    Add files via upload

--- Python Project/Codes/main.py
+++ Python Project/Codes/main.py
@@ -1,15 +1,33 @@
 from classes import *
 
-student_list = StudentList('CES3063_Fall2020_rptSinifListesi.XLS')
+test= GUI()
 
-Reports('zoom_poll_reports', student_list).read_reports()
+button = Button( text="SELECT CLASS LIST", command=test.open_dialog)
+test.config_button(button)
 
-results = Results(student_list.get_students())
 
-Attendance(student_list.get_students(), results).add_attendance()
+button2 = Button( text="LIST STUDENT ", command=test.StudentList)
+test.config_button(button2)
+'''
+button3 = Button( text="button", command=test.open_dialog)
+test.config_button(button3)
+'''
 
-answer_keys = AnswerKeys('answer_keys').get_answer_keys()
 
-CheckAnswers(student_list.get_students(), results, answer_keys).check()
+test.compileWindow()
 
-results.save_book()
+
+
+#student_list = StudentList('CES3063_Fall2020_rptSinifListesi.XLS') # exceli okur
+
+#Reports('zoom_poll_reports', student_list).read_reports() #poll bilgisi ni student list e ekliyor
+
+#results = Results(student_list.get_students())
+
+#Attendance(student_list.get_students(), results).add_attendance()
+
+#answer_keys = AnswerKeys('answer_keys').get_answer_keys()
+
+#CheckAnswers(student_list.get_students(), results, answer_keys).check()
+
+#results.save_book()
